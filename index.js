b80018a5522d13bee004b0dfc497ebd30acb996d
Kushal Lahoti <lahotikn18.comp@coep.ac.in>
2022-06-23 17:10:33 +0530

    Eight commit

--- index.js
+++ index.js
@@ -30,3 +30,26 @@ function showlessprojects() {
     document.getElementById("less-projects").style.display = 'none';
     document.getElementById("more-projects").style.display = 'block';
 }
+
+function showmorejourney() {
+    // let x = document.getElementsByClassName("col");
+    const boxes = document.querySelectorAll('.custom-col-journey');
+   
+
+    for (let i = 2; i < boxes.length; i++) {
+        boxes[i].style.display = 'block';
+      }
+
+    document.getElementById("more-journey").style.display = 'none';
+    document.getElementById("less-journey").style.display = 'block';
+}
+
+function showlessjourney() {
+    const boxes = document.querySelectorAll('.custom-col-journey');
+    for (let i = 2; i < boxes.length; i++) {
+        boxes[i].style.display = 'none';
+      }
+
+    document.getElementById("less-journey").style.display = 'none';
+    document.getElementById("more-journey").style.display = 'block';
+}
