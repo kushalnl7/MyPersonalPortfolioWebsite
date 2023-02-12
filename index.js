b867f88776fcf94aa49f38c7ccf50e4cdd575bb1
Kushal Lahoti <lahotikn18.comp@coep.ac.in>
2023-02-13 01:32:09 +0530

    Js file Updated

--- index.js
+++ index.js
@@ -64,41 +64,6 @@ function showlessjourney() {
     document.getElementById('journey').scrollIntoView();
 }
 
-function sendEmail() {
-    Email.send({
-      Host: "smtp.mailtrap.io",
-      Username: "webdevkl7@gmail.com",
-      Password: "Pati@1170234",
-      To: 'receiver@email_address.com',
-      From: "webdevkl7@gmail.com",
-      Subject: "Sending Email using javascript",
-      Body: "Well that was easy!!",
-    })
-      .then(function (message) {
-        alert("mail sent successfully")
-      });
-  }
-
-function validateEmail(email) 
-    {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-
-
-function Submit(){
-    let x = document.getElementById("myForm");
-    let check = 0;
-    if(x.name.value == "") check=check+1;
-    if(x.email.value == "") check=check+1;
-    if(!validateEmail(x.email.value)) check=check+1;
-    if(x.subject.value == "") check=check+1;
-    if(x.message.value == "") check=check+1;
-    // if(check != 0) return 0;
-    sendEmail();
-
-}
-
 window.addEventListener("#project", function () {
     window.scrollTo(window.scrollX, window.scrollY - 500);
 });
